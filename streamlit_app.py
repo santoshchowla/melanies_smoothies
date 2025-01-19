7189088f9f6dcf201a39efda4fb26656b7c1972e
santoshchowla <santoshchowla@eaton.com>
2025-01-19 12:54:53 -0500

    Update streamlit_app.py
    Changed order of conneciton stmt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-cnx =st.connection("snowflake")
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,7 +15,7 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
 
 
-
+cnx =st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
